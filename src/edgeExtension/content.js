077f8c993005baf6f7a8fdd58dd7d3c345e819a3
paulwilcox <thetahard@gmail.com>
2023-04-25 19:52:21 -0500

    Moved to hiding and fronting strategy
    Works on google news now.

--- src/edgeExtension/content.js
+++ src/edgeExtension/content.js
@@ -1,49 +1,66 @@
-document.addEventListener("contextmenu", function(event) {
+window.addEventListener("contextmenu", function(event) {
 
-  let target = event.target;
-  
-  if (target.tagName !== "IMG") 
-    return;
+  var x = event.clientX;
+  var y = event.clientY;
+  let img = 
+    document.elementsFromPoint(x,y)
+    .find(el => el.tagName == "IMG");
+
+  if (!img) 
+    return;  
 
   event.preventDefault();
+  
+  let cm = document.querySelector("#pickTamerContextMenu");
+  Object.assign(cm.style, {
+    top: event.pageY + "px",
+    left: event.pageX + "px",
+    display: "block"
+  });
 
-  let imageUrl = target.src;
-  
-  // Create your custom context menu
-  let contextMenu = document.createElement("div");
-  contextMenu.id = "pickTamerContextMenu";
-  contextMenu.style.position = "absolute";
-  contextMenu.style.top = event.pageY + "px";
-  contextMenu.style.left = event.pageX + "px";
-  contextMenu.style.backgroundColor = "white";
-  contextMenu.style.border = "1px solid black";
-  contextMenu.style.padding = "10px";
-  
-  let contextMenuItem = document.createElement("a");
-  contextMenuItem.textContent = "View Image";
-  contextMenuItem.href = imageUrl;
-  contextMenuItem.target = "_blank";
-  contextMenu.appendChild(contextMenuItem);
-  
-  document.body.appendChild(contextMenu);
-  
+  let cmItem = cm.querySelector("#viewImage");
+  cmItem.href = img.src;
+
 });
 
-document.addEventListener("mousedown", removeContextMenus)
+document.addEventListener("mousedown", hideConextMenu)
 document.addEventListener("keydown", event => {
   if (event.key === "Escape") 
-    removeContextMenus(event);
+    hideConextMenu(event);
 });
 
-function removeContextMenus (event) {
-
-  let contextMenus = document.querySelectorAll("#pickTamerContextMenu");
-  
-  for(let contextMenu of contextMenus)
-    // Check if the context menu is open and the click did not occur inside the context menu
-    if (contextMenu && !contextMenu.contains(event.target)) {
-      contextMenu.remove();
-      contextMenu = null;
-    }  
-
+function hideConextMenu (event) {
+  let contextMenu = document.querySelector("#pickTamerContextMenu");
+  // Check if the context menu is open and the click did not occur inside the context menu
+  if (contextMenu && !contextMenu.contains(event.target)) {
+    contextMenu.style.display = "none";
+  }
 }
+
+window.onload = function() {
+
+  let div = document.createElement("div");
+  div.id = "pickTamerContextMenu";
+  Object.assign(div.style, {
+    position: "absolute",
+    zIndex: 9999,
+    top: 0,
+    left: 0,
+    backgroundColor: "white",
+    border: "1px solid black",
+    padding: "10px",
+    display: "none"  
+  });
+  
+  let item = document.createElement("a");
+  Object.assign(item, {
+    id: "viewImage",
+    textContent: "View Image",
+    href: "https://www.google.com",
+    target: "_blank"
+  })
+
+  div.appendChild(item);
+  document.body.appendChild(div);
+
+};
